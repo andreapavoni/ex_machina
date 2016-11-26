10362ace4e169b8b615ededdb59358af62270d1d
Andrea Pavoni <andreapavoni@users.noreply.github.com>
2016-11-26 11:50:48 +0100

    Force ecto dependency to 2.1.0.rc.4

--- mix.exs
+++ mix.exs
@@ -36,8 +36,8 @@ defmodule ExMachina.Mixfile do
     [
       {:ex_doc, "~> 0.9", only: :dev},
       {:earmark, ">= 0.0.0", only: :dev},
-      {:ecto, "~> 2.0", optional: true},
-      {:ecto, "~> 2.0", only: [:dev, :test]},
+      {:ecto, "~> 2.1.0-rc.4", optional: true},
+      {:ecto, "~> 2.1.0-rc.4", only: [:dev, :test]},
       {:postgrex, ">= 0.0.0", only: [:test]},
       {:poison, "~> 2.0.0", only: :test}
     ]
